acb8baa62d3896c207c4cb1b45e800e57741d509
Alysson_UFC <alysson10araujo@gmail.com>
2021-03-18 20:30:40 -0300

    Atualização 18/03/2021

--- src/exec/Principal.java
+++ src/exec/Principal.java
@@ -2,8 +2,17 @@ package exec;
 
 import database.*;
 public class Principal {
-
+    public static void menu (){
+        System.out.println(" ###### AGENDA TELEFONICA ONLINE! ###### \n" +
+                "Digite 1 para mostrar todas as pessoas fisicas na agenda telefonica;                  \n" +
+                "Digite 2 para mostrar todas as pessoas juridicas na agenda telefonica;                \n" +
+                "Digite 3 para mostrar todos os usuarios presentes na agenda telefonica;               \n" +
+                "Digite 4 para buscar um estabelecimento, como emegencia,restaurante, universidade, " +
+                "entre outros;                                                                         \n" +
+                "" +
+                "");
+    }
     public static void main(String[] args){
-
+        menu();
     }
 }
